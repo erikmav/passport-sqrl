028264de226c005318b243ae22be0bd66601703a
Erik Mavrinac <erikma1@hotmail.com>
2017-10-05 18:27:28 -0700

    Add server friendly name support for URL factory

--- src/SqrlTests/SqrlUrl.tests.ts
+++ src/SqrlTests/SqrlUrl.tests.ts
@@ -6,44 +6,51 @@ import { SqrlUrl, SqrlUrlFactory } from '../passport-sqrl/SqrlUrl';
 describe('SqrlUrlFactory', () => {
   describe('StaticUrlCreationChecks', () => {
     it('should return the expected URL format from various parameter configurations passed to SqlUrlFactory.create()', () => {
-      let url = SqrlUrlFactory.create(false, 'foo.com', null, "secure1");
+      let url = SqrlUrlFactory.create(false, 'foo.com', "secure1");
       assert.equal(url, 'qrl://foo.com?nut=secure1');
 
-      url = SqrlUrlFactory.create(true, 'www.foo.com', '/sqrlLogin', "secure2");
+      url = SqrlUrlFactory.create(true, 'www.foo.com', "secure2", '/sqrlLogin');
       assert.equal(url, 'sqrl://www.foo.com/sqrlLogin?nut=secure2');
 
-      url = SqrlUrlFactory.create(true, 'www.foo.com', 'sqrlLogin2?', new Buffer("secure3"));
+      url = SqrlUrlFactory.create(true, 'www.foo.com', new Buffer("secure3"), 'sqrlLogin2?');
       assert.equal(url, 'sqrl://www.foo.com/sqrlLogin2?nut=secure3');
 
-      url = SqrlUrlFactory.create(true, 'www.foo.com', '', new Buffer("secure4"));
+      url = SqrlUrlFactory.create(true, 'www.foo.com', new Buffer("secure4"), '');
       assert.equal(url, 'sqrl://www.foo.com?nut=secure4');
 
-      url = SqrlUrlFactory.create(true, 'www.foo.com', '', new Buffer("secure5"), 1);
+      url = SqrlUrlFactory.create(true, 'www.foo.com', new Buffer("secure5"), '', 1);
       assert.equal(url, 'sqrl://www.foo.com?nut=secure5');
 
-      url = SqrlUrlFactory.create(true, 'www.foo.com', 'someuser', new Buffer("secure6"), 1);
+      url = SqrlUrlFactory.create(true, 'www.foo.com', new Buffer("secure6"), 'someuser', 1);
       assert.equal(url, 'sqrl://www.foo.com/someuser?nut=secure6&x=1');
 
-      url = SqrlUrlFactory.create(true, 'www.foo.com', 'someuser', new Buffer("secure7"), 1000);
+      url = SqrlUrlFactory.create(true, 'www.foo.com', new Buffer("secure7"), 'someuser', 1000);
       assert.equal(url, 'sqrl://www.foo.com/someuser?nut=secure7&x=9');
 
-      url = SqrlUrlFactory.create(true, 'www.foo.com', '/someuser?', new Buffer("secure8"), 1000);
+      url = SqrlUrlFactory.create(true, 'www.foo.com', new Buffer("secure8"), '/someuser?', 1000);
       assert.equal(url, 'sqrl://www.foo.com/someuser?nut=secure8&x=9');
+
+      url = SqrlUrlFactory.create(true, 'www.foo.com', new Buffer("secure9"), /*path*/undefined, /*domainExt*/undefined, "Foo");
+      assert.equal(url, 'sqrl://www.foo.com?nut=secure9&sfn=Rm9v');
     });
   });
 
   describe('FactoryCreationChecks', () => {
     it('should return the expected URL format from various parameter configurations passed to an instance of SqlUrlFactory', () => {
       let factory = new SqrlUrlFactory(true, 'foo.com', '/sqrlLogin');
-      let url = factory.createFromNut("secure1");
+      let url = factory.create("secure1");
       assert.equal(url, 'sqrl://foo.com/sqrlLogin?nut=secure1');
 
-      factory = new SqrlUrlFactory(false, 'foo.com', null);
-      url = factory.createFromNut("secure2");
+      factory = new SqrlUrlFactory(false, 'foo.com');
+      url = factory.create("secure2");
       assert.equal(url, 'qrl://foo.com?nut=secure2');
 
-      url = factory.createFromPathAndNut("/login", "secure2");
-      assert.equal(url, 'qrl://foo.com/login?nut=secure2');
+      url = factory.create("secure3", "/login");
+      assert.equal(url, 'qrl://foo.com/login?nut=secure3');
+
+      factory = new SqrlUrlFactory(true, 'foo.com', '/login', "Friendly!");
+      url = factory.create("secure4");
+      assert.equal(url, 'sqrl://foo.com/login?nut=secure4&sfn=RnJpZW5kbHkh');
     });
   });
 });
--- src/passport-sqrl/SqrlUrl.ts
+++ src/passport-sqrl/SqrlUrl.ts
@@ -1,3 +1,4 @@
+import * as base64url from 'base64-url';
 
 /**
  * Implements a SQRL URL generator and parser.
@@ -20,16 +21,18 @@ export class SqrlUrlFactory {
    * Creates a SQRL URL from full "nut" metadata.
    * @param secure Whether the server is using TLS, which maps to the 'sqrl://' or 'qrl://' schemes.
    * @param domain The site domain, e.g. "www.foo.com"
-   * @param pathString Optional path string, e.g. "path/to/sqrlLogin". May start with a forward slash.
    * @param serverNut The opaque, unique server data generated for this URL, passed as the nut= query parameter.
+   * @param pathString Optional path string, e.g. "path/to/sqrlLogin". May start with a forward slash.
    * @param domainExtension When positive, specifies the value to place into the x= query parameter that tells the client how many characters of the pathString to include in its server key hash.
+   * @param serverFriendlyName Optional server friendly name for display in the SQRL client. When specified, adds the sfn= query parameter.
    */
   public static create(
       secure: boolean,
       domain: string,
-      pathString: string | null,
       serverNut: string | Buffer,
-      domainExtension?: number)
+      pathString?: string,
+      domainExtension?: number,
+      serverFriendlyName?: string)
       : string {
     let scheme = secure ? 'sqrl' : 'qrl';
 
@@ -52,40 +55,40 @@ export class SqrlUrlFactory {
       domainExt = `&x=${domainExtension}`;
     }
 
-    return `${scheme}://${domain}${pathString}?nut=${serverNut}${domainExt}`;
+    let sfn = '';
+    if (serverFriendlyName && serverFriendlyName.length > 0) {
+      sfn = '&sfn=' + base64url.encode(serverFriendlyName);
+    }
+
+    return `${scheme}://${domain}${pathString}?nut=${serverNut}${domainExt}${sfn}`;
   }
 
   private secure: boolean;
   private domain: string;
-  private pathString: string | null;
+  private pathString: string | undefined;
+  private serverFriendlyName: string | undefined;
   
   /**
    * Creates a SQRL URL factory with static configuration information.
    * @param secure Whether the server is using TLS, which maps to the 'sqrl://' or 'qrl://' schemes.
    * @param domain The site domain, e.g. "www.foo.com"
    * @param pathString Optional path string, e.g. "path/to/sqrlLogin". May start with a forward slash.
+   * @param serverFriendlyName Optional server friendly name for display in the SQRL client. When specified, adds the sfn= query parameter.
    */
-  constructor(secure: boolean, domain: string, pathString: string | null) {
+  constructor(secure: boolean, domain: string, pathString?: string, serverFriendlyName?: string) {
     this.secure = secure;
     this.domain = domain;
     this.pathString = pathString;
+    this.serverFriendlyName = serverFriendlyName;
   }
 
   /**
    * Creates a SQRL URL from the provided unique server data.
    * @param serverNut The opaque, unique server data generated for this URL, passed as the nut= query parameter.
-   */
-  public createFromNut(serverNut: string | Buffer): string {
-    return SqrlUrlFactory.create(this.secure, this.domain, this.pathString, serverNut);
-  }
-
-  /**
-   * Creates a SQRL URL from the provided unique server data.
-   * @param pathString Path string, e.g. "path/to/sqrlLogin". May start with a forward slash.
-   * @param serverNut The opaque, unique server data generated for this URL, passed as the nut= query parameter.
+   * @param pathString Optional path string, e.g. "path/to/sqrlLogin". May start with a forward slash.
    * @param domainExtension When positive, specifies the value to place into the x= query parameter that tells the client how many characters of the pathString to include in its server key hash.
    */
-  public createFromPathAndNut(pathString: string, serverNut: string | Buffer, domainExtension?: number): string {
-    return SqrlUrlFactory.create(this.secure, this.domain, pathString, serverNut, domainExtension);
+  public create(serverNut: string | Buffer, pathString?: string, domainExtension?: number): string {
+    return SqrlUrlFactory.create(this.secure, this.domain, serverNut, pathString || this.pathString, domainExtension, this.serverFriendlyName);
   }
 }
